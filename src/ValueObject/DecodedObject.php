efeb8398bd12764bc575ef6949ac3b6e7f266ee1
Liam Hackett <liamjhackett@gmail.com>
2023-09-30 22:23:31 +0100

    Add first pass of Docblock generator for Decoded Objects

--- src/ValueObject/DecodedObject.php
+++ src/ValueObject/DecodedObject.php
@@ -2,6 +2,8 @@
 
 namespace LiamH\Valueobjectgenerator\ValueObject;
 
+use LiamH\Valueobjectgenerator\Enum\ParameterType;
+
 readonly class DecodedObject
 {
 
@@ -11,8 +13,60 @@ readonly class DecodedObject
      */
     public function __construct(
         public string $name,
-        public array  $parameters,
+        public array $parameters,
     ) {
     }
 
+    /**
+     * @return DecodedObject[]
+     */
+    public function getChildObjects(): array
+    {
+        $objects = [];
+
+        foreach ($this->parameters as $parameter) {
+            if ($parameter->hasObject()) {
+                $objects = [...$objects, ...$parameter->getObjects()];
+            }
+        }
+
+        return $objects;
+    }
+
+    public function generateDocblock(): string
+    {
+        $hasDocblock = false;
+        $docblock = '**';
+
+        foreach ($this->parameters as $parameter) {
+            if (is_array($parameter->arrayTypes) && $parameter->hasType(ParameterType::ARRAY)) {
+                $hasDocblock = true;
+                $docblock .= '\n@var ';
+
+                foreach ($parameter->arrayTypes as $key => $type) {
+                    if ($key > 0) {
+                        $docblock .= '|';
+                    }
+
+                    if ($type instanceof self) {
+                        $docblock .= $type->name . '[]';
+                        continue;
+                    }
+
+                    if ($type instanceof ParameterType) {
+                        $docblock .= $type->getDefinitionName() . '[]';
+                    }
+                }
+
+                $docblock .= ' $' . $parameter->formattedName;
+            }
+        }
+
+        if (!$hasDocblock) {
+            return '';
+        }
+
+        $docblock .= '/n**';
+        return $docblock;
+    }
 }
